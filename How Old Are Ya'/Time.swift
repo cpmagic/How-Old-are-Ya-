edd312cea6569d512c1f0fe971fc8932c44946d1
Tom Hill <thill@cpmagic.com>
2018-10-20 15:33:31 -0400

    first commit

---
+++ How Old Are Ya'/Time.swift
@@ -0,0 +1,9 @@
+//
+//  Time.swift
+//  How Old Are Ya'
+//
+//  Created by Thomas Hill on 10/20/18.
+//  Copyright © 2018 Computer Magic. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# How-Old-are-Ya-
